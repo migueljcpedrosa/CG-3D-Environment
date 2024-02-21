699f699381e132fef2033fe8778e4b8e1b2a30d2
migueljcpedrosa <migueljcpedrosa@gmail.com>
2024-02-21 13:23:06 +0000

    Defined top and bottom views

--- tp2/MyUnitCubeQuad.js
+++ tp2/MyUnitCubeQuad.js
@@ -13,31 +13,45 @@ export class MyUnitCubeQuad extends CGFobject {
 
     display() {
         //Right view
-        this.scene.pushMatrix()
-        this.scene.translate(0.5, 0, 0)
-        this.scene.rotate(Math.PI / 2, 0, 1, 0) //rotate around y axis
-        this.quad.display()
-        this.scene.popMatrix()
+        this.scene.pushMatrix();
+        this.scene.translate(0.5, 0, 0);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0); //rotate around y axis
+        this.quad.display();
+        this.scene.popMatrix();
 
         //Left view
-        this.scene.pushMatrix()
-        this.scene.translate(-0.5, 0, 0)
-        this.scene.rotate(3 * Math.PI / 2, 0, 1, 0)
-        this.quad.display()
-        this.scene.popMatrix()
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0, 0);
+        this.scene.rotate(3 * Math.PI / 2, 0, 1, 0);
+        this.quad.display();
+        this.scene.popMatrix();
 
         //Front view
-        this.scene.pushMatrix()
-        this.scene.translate(0, 0, 0.5)
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5);
         //no need to rotate
-        this.quad.display()
-        this.scene.popMatrix()
+        this.quad.display();
+        this.scene.popMatrix();
 
         //Back view
-        this.scene.pushMatrix()
-        this.scene.translate(0,0,-0.5)
-        this.scene.rotate(Math.PI,1, 0, 0) //rotate 180 degrees. quad only visible from one side
-        this.quad.display()
-        this.scene.popMatrix()
+        this.scene.pushMatrix();
+        this.scene.translate(0,0,-0.5);
+        this.scene.rotate(Math.PI, 1, 0, 0); //rotate 180 degrees. quad only visible from one side
+        this.quad.display();
+        this.scene.popMatrix();
+
+        //Top view
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0.5, 0);
+        this.scene.rotate(-1 * Math.PI/2, 1, 0, 0);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        //Bottom view
+        this.scene.pushMatrix();
+        this.scene.translate(0, -0.5, 0);
+        this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.quad.display();
+        this.scene.popMatrix();
     }
 }
