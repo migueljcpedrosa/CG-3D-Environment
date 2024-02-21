2131d53a1f3fc843c48bf20a1290d15015fe9054
migueljcpedrosa <migueljcpedrosa@gmail.com>
2024-02-21 13:03:43 +0000

    Defined front view in MyUnitCubeQuad's display method

--- tp2/MyUnitCubeQuad.js
+++ tp2/MyUnitCubeQuad.js
@@ -1,13 +1,22 @@
 import { CGFobject } from '../lib/CGF.js';
+import { MyQuad } from './MyQuad.js';
 /**
- * MyQuad
+ * MyUnitCubeQuad
  * @constructor
  * @param scene - Reference to MyScene object
  */
 export class MyUnitCubeQuad extends CGFobject {
     constructor(scene) {
         super(scene);
-        this.initBuffers();
+        this.quad = new MyQuad(scene);
     }
 
+    display() {
+        //Front view
+        this.scene.pushMatrix()
+        this.scene.translate(0.5, 0, 0)
+        this.scene.rotate(Math.PI / 2, 0, 1, 0)
+        this.quad.display()
+        this.scene.popMatrix()
+    }
 }
