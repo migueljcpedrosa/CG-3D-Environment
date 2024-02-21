81ecb91f287520de5c011f4723ec433c40605e5f
migueljcpedrosa <migueljcpedrosa@gmail.com>
2024-02-21 13:25:23 +0000

    Exercise 4.2 is complete

--- tp2/MyUnitCubeQuad.js
+++ tp2/MyUnitCubeQuad.js
@@ -12,20 +12,6 @@ export class MyUnitCubeQuad extends CGFobject {
     }
 
     display() {
-        //Right view
-        this.scene.pushMatrix();
-        this.scene.translate(0.5, 0, 0);
-        this.scene.rotate(Math.PI / 2, 0, 1, 0); //rotate around y axis
-        this.quad.display();
-        this.scene.popMatrix();
-
-        //Left view
-        this.scene.pushMatrix();
-        this.scene.translate(-0.5, 0, 0);
-        this.scene.rotate(3 * Math.PI / 2, 0, 1, 0);
-        this.quad.display();
-        this.scene.popMatrix();
-
         //Front view
         this.scene.pushMatrix();
         this.scene.translate(0, 0, 0.5);
@@ -51,6 +37,19 @@ export class MyUnitCubeQuad extends CGFobject {
         this.scene.pushMatrix();
         this.scene.translate(0, -0.5, 0);
         this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.quad.display();
+        this.scene.popMatrix();
+        //Right view
+        this.scene.pushMatrix();
+        this.scene.translate(0.5, 0, 0);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0); //rotate around y axis
+        this.quad.display();
+        this.scene.popMatrix();
+
+        //Left view
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0, 0);
+        this.scene.rotate(3 * Math.PI / 2, 0, 1, 0);
         this.quad.display();
         this.scene.popMatrix();
     }
