8c6f1441696e7f4bd19d108778cc4df3dcf5d927
Miguel Pedrosa <93871576+migueljcpedrosa@users.noreply.github.com>
2024-03-03 22:14:02 +0000

    Exercise 8Part2 done

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -1,6 +1,6 @@
 import { CGFobject } from '../lib/CGF.js';
 /**
- * MyDiamond
+ * MyPrism
  * @constructor
  * @param scene - Reference to MyScene object
  */
@@ -13,46 +13,53 @@ export class MyPrism extends CGFobject {
     }
 
     initBuffers() {
-        
+        //initialize arrays
         this.vertices = [];
         this.indices = [];
         this.normals = [];
 
+        //slice cycle
         let index = 0;
         let incrementAngle = 2 * Math.PI / this.slices;
-        for (let sliceIterator = 0 ; sliceIterator < this.slices ; sliceIterator++) {
 
-            //Point1 (beggining of this slice)
-            let x1 = Math.cos(sliceIterator*incrementAngle);
-            let y1 = Math.sin(sliceIterator*incrementAngle);
+        let incrementStacks = 1 / this.stacks;
 
-            //Point2 (end of this slice, beggining of next slice)
-            let x2 = Math.cos((sliceIterator+1)*incrementAngle);
-            let y2 = Math.sin((sliceIterator+1)*incrementAngle);
+        for (let stackIterator = 0 ; stackIterator < this.stacks ; stackIterator++) {
+            for (let sliceIterator = 0 ; sliceIterator < this.slices ; sliceIterator++) {
+
+                //Point1 (beggining of this slice)
+                let x1 = Math.cos(sliceIterator*incrementAngle);
+                let y1 = Math.sin(sliceIterator*incrementAngle);
+
+                //Point2 (end of this slice, beggining of next slice)
+                let x2 = Math.cos((sliceIterator+1)*incrementAngle);
+                let y2 = Math.sin((sliceIterator+1)*incrementAngle);
+                
+                //Point between Point1 and Point2 (in circle of the same radius as Point1 and Point2)
+                let x = Math.cos((sliceIterator+0.5)* incrementAngle);
+                let y = Math.sin((sliceIterator+0.5)*incrementAngle);
+
+                let size = Math.sqrt(x*x + y*y); 
+                this.vertices.push(x1, y1, stackIterator * incrementStacks,
+                                   x2, y2, stackIterator * incrementStacks, 
+                                   x1, y1, (stackIterator + 1) * incrementStacks, 
+                                   x2, y2, (stackIterator + 1) * incrementStacks);
+
+                this.indices.push(index+2, index, index+1,    //triangle1
+                                  index+1, index+3, index+2); //triangle2
+
+                //ivide by size to normalize the vector
+                this.normals.push(x/size, y/size, 0,
+                                x/size, y/size, 0,
+                                x/size, y/size, 0, 
+                                x/size, y/size, 0);
+
+                index += 4; //each slice has 4 vertices (start in index and end in index+3)
             
-            //Intermediate point in slice 
-            let x = Math.cos((sliceIterator+0.5)* incrementAngle);
-            let y = Math.sin((sliceIterator+0.5)*incrementAngle);
-            let size = Math.sqrt(x*x + y*y);
+            }
 
-            this.vertices.push(x1, y1, 0,
-                               x2, y2, 0, 
-                               x1, y1, 1, 
-                               x2, y2, 1);
-
-            this.indices.push(index+2, index, index+1,    //triangle1
-                              index+1, index+3, index+2); //triangle2
-
-            this.normals.push(x/size, y/size, 0,
-                              x/size, y/size, 0, 
-                              x/size, y/size, 0, 
-                              x/size, y/size, 0);
-
-            index+=4; //each slice has 4 vertices (strt in index and end in index+3)
-           
+            this.primitiveType = this.scene.gl.TRIANGLES;
+            this.initGLBuffers();
         }
-
-        this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();
     }
 }
