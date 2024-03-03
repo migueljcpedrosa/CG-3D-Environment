19af5758447c8db5c9688867e9d532db8d02f2c9
Miguel Pedrosa <93871576+migueljcpedrosa@users.noreply.github.com>
2024-03-03 19:55:35 +0000

    Added comments

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -22,22 +22,33 @@ export class MyPrism extends CGFobject {
         let incrementAngle = 2 * Math.PI / this.slices;
         for (let sliceIterator = 0 ; sliceIterator < this.slices ; sliceIterator++) {
 
-            //Point1
+            //Point1 (beggining of this slice)
             let x1 = Math.cos(sliceIterator*incrementAngle);
             let y1 = Math.sin(sliceIterator*incrementAngle);
 
-            //Point2
+            //Point2 (end of this slice, beggining of next slice)
             let x2 = Math.cos((sliceIterator+1)*incrementAngle);
             let y2 = Math.sin((sliceIterator+1)*incrementAngle);
             
+            //Intermediate point in slice 
             let x = Math.cos((sliceIterator+0.5)* incrementAngle);
             let y = Math.sin((sliceIterator+0.5)*incrementAngle);
             let size = Math.sqrt(x*x + y*y);
 
-            this.vertices.push(x1, y1, 0, x2, y2, 0, x1, y1, 1, x2, y2, 1);
-            this.indices.push(index+2, index, index+1, index+1, index+3, index+2);
-            this.normals.push(x/size, y/size, 0, x/size, y/size, 0, x/size, y/size, 0, x/size, y/size, 0);
-            index+=4;
+            this.vertices.push(x1, y1, 0,
+                               x2, y2, 0, 
+                               x1, y1, 1, 
+                               x2, y2, 1);
+
+            this.indices.push(index+2, index, index+1,    //triangle1
+                              index+1, index+3, index+2); //triangle2
+
+            this.normals.push(x/size, y/size, 0,
+                              x/size, y/size, 0, 
+                              x/size, y/size, 0, 
+                              x/size, y/size, 0);
+
+            index+=4; //each slice has 4 vertices (strt in index and end in index+3)
            
         }
 
