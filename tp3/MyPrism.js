890f3d78bfad0bdaa2e0af2f540380fe044ac72b
Miguel Pedrosa <93871576+migueljcpedrosa@users.noreply.github.com>
2024-03-03 18:15:37 +0000

    Updated vertices, indices and normals initialization arrays

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -13,19 +13,12 @@ export class MyPrism extends CGFobject {
     }
 
     initBuffers() {
-        this.vertices = [
-            0.5, -0.5, 0,	    //0 
-            0.5, 0.5, 0,	    //1
-            -0.5, 0.5, 0,	    //2
-            -0.5, -0.5, 0,	    //3
-        ];
+        this.vertices = [];
 
         //Counter-clockwise reference of vertices
-        this.indices = [
-            0, 1, 3,
-            3, 1, 2
-        ];
+        this.indices = [];
 
+        this.normals = [];
         //The defined indices (and corresponding vertices)
         //will be read in groups of three to draw triangles
         this.primitiveType = this.scene.gl.TRIANGLES;
