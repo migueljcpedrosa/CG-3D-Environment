13643ca26f3bf14b882fec69dd6d0b28179464ea
Miguel Pedrosa <93871576+migueljcpedrosa@users.noreply.github.com>
2024-03-13 11:03:51 +0000

    Added MyUnitCubeQuad.js to tp4

---
+++ tp4/MyUnitCubeQuad.js
@@ -0,0 +1,62 @@
+import { CGFobject } from '../lib/CGF.js';
+import { MyQuad } from './MyQuad.js';
+/**
+ * MyUnitCubeQuad
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyUnitCubeQuad extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.quad = new MyQuad(scene);
+    }
+
+    display() {
+        this.scene.pushMatrix();
+        this.scene.setDiffuse(0.1, 0.7, 1, 1.0);
+        this.scene.setAmbient(1, 1, 1, 1.0);
+
+        //Front view
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5);
+        //no need to rotate
+        this.quad.display();
+        this.scene.popMatrix();
+
+        //Back view
+        this.scene.pushMatrix();
+        this.scene.translate(0,0,-0.5);
+        this.scene.rotate(Math.PI, 1, 0, 0); //rotate 180 degrees. quad only visible from one side
+        this.quad.display();
+        this.scene.popMatrix();
+
+        //Top view
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0.5, 0);
+        this.scene.rotate(-1 * Math.PI/2, 1, 0, 0);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        //Bottom view
+        this.scene.pushMatrix();
+        this.scene.translate(0, -0.5, 0);
+        this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.quad.display();
+        this.scene.popMatrix();
+        //Right view
+        this.scene.pushMatrix();
+        this.scene.translate(0.5, 0, 0);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0); //rotate around y axis
+        this.quad.display();
+        this.scene.popMatrix();
+
+        //Left view
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0, 0);
+        this.scene.rotate(3 * Math.PI / 2, 0, 1, 0);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+    }
+}
