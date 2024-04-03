3af9bdd7023d1c5441285cdfc20eda9ae774dca7
Gonçalo Marques <goncalocarvalho.marques@gmail.com>
2024-04-03 12:16:32 +0100

    sphere with earth texture

--- project/MyScene.js
+++ project/MyScene.js
@@ -1,5 +1,6 @@
 import { CGFscene, CGFcamera, CGFaxis, CGFappearance, CGFshader, CGFtexture } from "../lib/CGF.js";
 import { MyPlane } from "./MyPlane.js";
+import { MySphere } from "./MySphere.js";
 
 /**
  * MyScene
@@ -26,6 +27,7 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.plane = new MyPlane(this,30);
+    this.sphere = new MySphere(this, 80, 40);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -34,9 +36,15 @@ export class MyScene extends CGFscene {
     this.enableTextures(true);
 
 this.texture = new CGFtexture(this, "images/terrain.jpg");
+this.earth = new CGFtexture(this, "images/earth.jpg");
 this.appearance = new CGFappearance(this);
 this.appearance.setTexture(this.texture);
 this.appearance.setTextureWrap('REPEAT', 'REPEAT');
+this.sphereAppearance = new CGFappearance(this);
+this.sphereAppearance.setTexture(this.earth);
+this.sphereAppearance.setTextureWrap('REPEAT', 'REPEAT');
+this.sphereAppearance.setAmbient(1, 1, 1, 1);
+this.sphereAppearance.setSpecular(1, 1, 1, 1);
 
   }
   initLights() {
@@ -71,6 +79,7 @@ this.appearance.setTextureWrap('REPEAT', 'REPEAT');
     // Apply transformations corresponding to the camera position relative to the origin
     this.applyViewMatrix();
 
+    this.setGlobalAmbientLight(1, 1, 1, 1);
     // Draw axis
     if (this.displayAxis) this.axis.display();
 
@@ -83,6 +92,8 @@ this.appearance.setTextureWrap('REPEAT', 'REPEAT');
     this.rotate(-Math.PI/2.0,1,0,0);
     this.plane.display();
     this.popMatrix();
+    this.sphereAppearance.apply();
+    this.sphere.display();
 
     // ---- END Primitive drawing section
   }
---
+++ project/MySphere.js
@@ -0,0 +1,56 @@
+import { CGFobject } from '../lib/CGF.js';
+/**
+ * MySphere
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MySphere extends CGFobject {
+    constructor(scene, slices, stacks) {
+        super(scene);
+        this.slices = slices;
+        this.stacks = stacks;
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
+
+        for (let stack = 0; stack <= this.stacks; stack++) {
+            let theta = stack * Math.PI / this.stacks;
+            let sinTheta = Math.sin(theta);
+            let cosTheta = Math.cos(theta);
+
+            for (let slice = 0; slice <= this.slices; slice++) {
+                let phi = slice * 2 * Math.PI / this.slices;
+                let sinPhi = Math.sin(phi);
+                let cosPhi = Math.cos(phi);
+
+                let x = cosPhi * sinTheta;
+                let y = cosTheta;
+                let z = sinPhi * sinTheta;
+                let u = 1 - (slice / this.slices);
+                let v = 1 - (stack / this.stacks);
+
+                this.normals.push(x, y, z);
+                this.texCoords.push(u, v);
+                this.vertices.push(x, y, z);
+            }
+        }
+
+        for (let stack = 0; stack < this.stacks; stack++) {
+            for (let slice = 0; slice < this.slices; slice++) {
+                let first = (stack * (this.slices + 1)) + slice;
+                let second = first + this.slices + 1;
+
+                this.indices.push(first, second, first + 1);
+                this.indices.push(second, second + 1, first + 1);
+            }
+        }
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+}
